98e2b8404dcc9c214afeb94acb790668b8ee3cb9
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2025-10-16 12:48:56 -0500

    Pensando en como hacer el inventario

--- inventario/models.py
+++ inventario/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class Inventario_Cierre(models.Model):
+class inv_base(models.Model):
+    """Base de inventario."""
     insumo = models.CharField(max_lenght=100, null=False)
     unidades = models.CharField(max_lenght=100)
     gramos = models.CharField(max_lenght=100)
+    ml = models.CharField(max_lenght=100)
 
 
+    def Descontar():
+        """Se va a encargar de descontar de los insumos"""
 
