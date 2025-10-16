a1d1e2de40df0e32870035d975fe867b72f7c8b5
xXJuanDavidXx <juandavidjaramillo456@gmail.com>
2025-10-16 08:37:45 -0500

    Variables de entorno

--- Pizza6K/settings.py
+++ Pizza6K/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)t8u-g=u8^)@$ep^6*$xod!^n@-h-18vl6f3+9)%tmoa8%5jpx'
-
+SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
